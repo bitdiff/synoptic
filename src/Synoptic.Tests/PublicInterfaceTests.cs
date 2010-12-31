59c821701556890569ddec21d43915a2ad04c28d
Ziya Suzen <ziya@suzen.net#>
2010-12-31 03:04:08 +0000

    public interface listing improvements

--- src/Synoptic.Tests/PublicInterfaceTests.cs
+++ src/Synoptic.Tests/PublicInterfaceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
 
@@ -17,13 +18,52 @@ namespace Synoptic.Tests
                 if (!type.IsPublic) continue;
                 if (!type.FullName.StartsWith("Synoptic.")) continue;
 
-                Console.WriteLine("public Type [{0}]", type.FullName);
+                Console.WriteLine(type.FullName);
+                Console.WriteLine("{");
+
                 foreach (var memberInfo in type.GetMembers())
                 {
-                    if (memberInfo.DeclaringType == type)
-                        Console.WriteLine("  public {0} [{1}]", memberInfo.MemberType, memberInfo.Name);
+                    if (memberInfo.DeclaringType != type) continue;
+                    if (memberInfo.MemberType == MemberTypes.Method && (memberInfo.Name.StartsWith("get_") || memberInfo.Name.StartsWith("set_"))) continue;
+
+                    if (memberInfo.MemberType == MemberTypes.Constructor)
+                    {
+                        var constructorInfo = memberInfo as ConstructorInfo;
+                        Console.WriteLine("  {0}({1})", type.Name, GetS(constructorInfo));
+                    }
+                    else if (memberInfo.MemberType == MemberTypes.Method)
+                    {
+                        var methodInfo = memberInfo as MethodInfo;
+
+                        string gen = "";
+                        string s = string.Join(", ", methodInfo.GetGenericArguments().Select(g => g.Name).ToArray());
+                        if (!string.IsNullOrEmpty(s)) gen = "<" + s + ">";
+
+                        Console.WriteLine("  {0} {1}{2}({3})", methodInfo.ReturnType.Name, methodInfo.Name, gen, GetS(methodInfo));
+                    }
+                    else if (memberInfo.MemberType == MemberTypes.Property)
+                    {
+                        var propertyInfo = memberInfo as PropertyInfo;
+                        
+                        string getter = type.GetMethods().Any(m => m.Name == "get_" + memberInfo.Name) ? "get; " : "";
+                        string setter = type.GetMethods().Any(m => m.Name == "set_" + memberInfo.Name) ? "set; " : ""; ;
+                        Console.WriteLine("  {0} {1} {{ {2}{3}}}", propertyInfo.PropertyType.Name, memberInfo.Name, getter, setter);
+                    }
+                    else
+                    {
+                        Console.WriteLine("  {0} [{1}]", memberInfo.Name, memberInfo.MemberType);
+                    }
                 }
+
+                Console.WriteLine("}");
+                Console.WriteLine();
             }
+        }
+
+        private string GetS(MethodBase constructorInfo)
+        {
+            return string.Join(", ", constructorInfo.GetParameters().Select(p => p.ParameterType.Name + " " + p.Name).
+                                         ToArray());
         }
     }
 }
