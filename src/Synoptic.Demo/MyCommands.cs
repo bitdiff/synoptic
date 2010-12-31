3e26084331d211e315f82a1f6c53ca39b4ae9a46
Ziya Suzen <ziya@suzen.net#>
2010-12-31 01:13:22 +0000

    added demo program

---
+++ src/Synoptic.Demo/MyCommands.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Synoptic.Demo
+{
+    [CommandSet]
+    public class MyCommands
+    {
+        [Command]
+        public void MyCommand(string param1)
+        {
+            Console.WriteLine("MyCommand");
+            Console.WriteLine("  param1=" + param1);
+        }
+
+        [Command]
+        public void MyCommand2(string camelParamOne)
+        {
+            Console.WriteLine("MyCommand2");
+            Console.WriteLine("  param1=" + camelParamOne);
+        }
+
+        [Command]
+        public void MyFourthCommand(Uri myUri)
+        {
+
+        }
+
+        [Command(Name = "ex")]
+        public void MyExceptionGenerator()
+        {
+            throw new ApplicationException("bmc");
+        }
+
+        [Command(Description = "this is another test command")]
+        public void MyThirdCommand([CommandParameter(Name = "lobster")] string param1, int? param2, [CommandParameter(Description = "this is a test parameter")] bool param3)
+        {
+            Console.WriteLine("MyCommand");
+            Console.WriteLine("  param1=" + param1);
+            Console.WriteLine("  param2=" + param2.HasValue);
+            Console.WriteLine("  param3" + param3);
+        }
+    }
+}
--- src/Synoptic.Demo/Program.cs
+++ src/Synoptic.Demo/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+            new CommandRunner().Run(args);
         }
     }
 }
