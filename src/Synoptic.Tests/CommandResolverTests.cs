b1fafe94e01a8bde71be5de434b16513919c10d2
Ziya Suzen <ziya@suzen.net#>
2010-12-31 00:59:31 +0000

    initial test project import

---
+++ src/Synoptic.Tests/CommandResolverTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ConsoleWrapper.Synoptic;
+
+using NUnit.Framework;
+
+namespace ConsoleWrapper.Test.Synoptic
+{
+
+    [TestFixture]
+    public class CommandResolverTests
+    {
+        [Test]
+        public void shoud_resolve_command()
+        {
+            CommandManifest manifest = new CommandFinder().FindInType(typeof(Test1));
+
+            var commandLineParseResult = new CommandLineParseResult(new List<CommandLineParameter> { }, new[] { "my-command" });
+            var commandName = CommandNameExtractor.Extract(new[] {commandLineParseResult});
+            Command command = new CommandResolver().Resolve(manifest, commandName);
+
+            Assert.IsNotNull(command);
+            Assert.That(command.Name, Is.EqualTo("MyCommand"));
+        }
+
+        public class Test1
+        {
+            [Command]
+            public void MyCommand()
+            {
+            }
+        }
+    }
+}
