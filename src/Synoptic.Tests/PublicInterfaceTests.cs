280bad9fd7a54b8ab8936ce9a161776ece18ac3e
Ziya Suzen <ziya@suzen.net#>
2010-12-31 01:43:35 +0000

    list public interfaces

---
+++ src/Synoptic.Tests/PublicInterfaceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Synoptic.Tests
+{
+    [TestFixture]
+    public class PublicInterfaceTests
+    {
+        [Test]
+        public void should_list_public_interfaces()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof (CommandRunner));
+
+            foreach (var type in assembly.GetTypes())
+            {
+                if (!type.IsPublic) continue;
+
+                Console.WriteLine("public Type [{0}]", type.FullName);
+                foreach (var memberInfo in type.GetMembers())
+                {
+                    if (memberInfo.DeclaringType == type)
+                        Console.WriteLine("  public {0} [{1}]", memberInfo.MemberType, memberInfo.Name);
+                }
+            }
+        }
+    }
+}
