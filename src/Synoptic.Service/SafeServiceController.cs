0d2ab4d8ec350f706f2a556903ebc61ea2f2a96e
Gavin Dau <g@bitdiff.com>
2011-05-04 11:12:17 +0100

    Change servicecontroller to have safe methods

---
+++ src/Synoptic.Service/SafeServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace Synoptic.Service
+{
+    public class SafeServiceController
+    {
+        public void Stop(string name)
+        {
+            var service = new ServiceController(name);
+            if (service.Status == ServiceControllerStatus.Stopped)
+                return;
+
+            service.WaitForStatus(ServiceControllerStatus.Stopped, new TimeSpan(0, 0, 0, 30));
+        }
+
+        public void Start(string name)
+        {
+            var service = new ServiceController(name);
+            if (service.Status == ServiceControllerStatus.Running)
+                return;
+
+            service.Start();
+        }
+    }
+}
