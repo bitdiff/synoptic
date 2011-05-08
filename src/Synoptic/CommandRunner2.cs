cd5459debc63d995f18b745cf629c2c9d4f7f252
Gavin Dau <g@bitdiff.com>
2011-05-08 21:05:21 +0100

    More playing

--- src/Synoptic/CommandRunner2.cs
+++ src/Synoptic/CommandRunner2.cs
@@ -66,6 +66,8 @@ namespace Synoptic
                 {
                     pipeline.Add(m);
                 }
+
+//                pipeline.Finally(pipeline.Execute);
                 
                 var response = pipeline.Execute(new Request(args));
                 response.Execute(false);
@@ -173,6 +175,18 @@ namespace Synoptic
 //            : this(new ActivatorServiceProvider())
         { }
 
+        public Pipeline<T, TOut> Add(Type filterType)
+        {
+            Add((IMiddleware<T, TOut>)_serviceProvider.GetService(filterType));
+            return this;
+        }
+
+        public Pipeline<T, TOut> Add<TFilter>() where TFilter : IMiddleware<T, TOut>
+        {
+            Add(typeof(TFilter));
+            return this;
+        }
+
         public int Count
         {
             get { return _filters.Count; }
@@ -202,5 +216,70 @@ namespace Synoptic
             : base("Next filter does not exist."
             + " Use 'Finally' or a filter that short-circuits before reaching the end of the chain")
         { }
+    }
+
+    public static class AssemblyExtensions
+    {
+        public static Pipeline<T, TOut> AddFiltersIn<T, TOut>(this Pipeline<T, TOut> pipeline, Assembly assembly)
+        {
+            return AddFiltersIn(pipeline, Assembly.GetCallingAssembly(), t => true);
+        }
+
+        public static Pipeline<T, TOut> AddFiltersIn<T, TOut>(this Pipeline<T, TOut> pipeline, string filterNamespace)
+        {
+            return AddFiltersIn(pipeline, Assembly.GetCallingAssembly(), filterNamespace);
+        }
+
+        public static Pipeline<T, TOut> AddFiltersIn<T, TOut>(
+            this Pipeline<T, TOut> pipeline,
+            Assembly assembly,
+            string filterNamespace)
+        {
+            return AddFiltersIn(pipeline, assembly, t => t.Namespace == filterNamespace);
+        }
+
+        public static Pipeline<T, TOut> AddFiltersIn<T, TOut>(
+            this Pipeline<T, TOut> pipeline,
+            Assembly assembly,
+            Func<Type, bool> predicate)
+        {
+            var filterTypes = assembly
+                .GetTypes()
+                .Where(t => typeof(IMiddleware<T, TOut>).IsAssignableFrom(t));
+
+            foreach (var filterType in filterTypes.Where(predicate))
+            {
+                pipeline.Add(filterType);
+            }
+
+            return pipeline;
+        }
+    }
+
+    public static class PipelineExtensions
+    {
+        /// <summary>
+        /// Convenience method to add a short-circuiting filter to the end of the chain
+        /// </summary>
+        public static Pipeline<T, TOut> Finally<T, TOut>(this Pipeline<T, TOut> pipeline, Func<T, TOut> func)
+        {
+            pipeline.Add(new ShortCircuit<T, TOut>(func));
+            return pipeline;
+        }
+
+        private class ShortCircuit<T, TOut> : IMiddleware<T, TOut>
+        {
+            private readonly Func<T, TOut> _func;
+
+            public ShortCircuit(Func<T, TOut> func)
+            {
+                this._func = func;
+            }
+
+            public TOut Process(T context, Func<T, TOut> executeNext)
+            {
+                return _func.Invoke(context);
+            }
+        }
     }
 }
