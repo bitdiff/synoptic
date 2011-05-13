4954b865e9f5fe768d97fc9c0b0a374896ec4d3f
Gavin Dau <g@bitdiff.com>
2011-05-13 09:36:13 +0100

    Refactoring help

--- src/Synoptic/CommandRunner.cs
+++ src/Synoptic/CommandRunner.cs
@@ -42,7 +42,7 @@ namespace Synoptic
 
             if (arguments.Count == 0)
             {
-                ShowCommandHelp(_optionSet);
+                new CommandLineHelpGenerator().ShowCommandHelp(_availableCommands, _optionSet);
                 return;
             }
 
@@ -87,59 +87,11 @@ namespace Synoptic
             _error.WriteLine(exception.Message);
         }
 
-        private void ShowCommandHelp(OptionSet optionSet)
-        {
-            var processName = Process.GetCurrentProcess().ProcessName;
-            var usagePreamble = String.Format("Usage: {0} ", processName);
-            Out.WordWrap(usagePreamble);
-            
-            var usagePattern = GetUsagePattern(optionSet);
-            Out.WordWrap(usagePattern, usagePreamble.Length);
-            Console.WriteLine("\n");
-
-            const int spacingWidth = 3;
-
-            var spacer = new string(' ', spacingWidth);
-            var maximumCommandNameLength = _availableCommands.Count() > 0 ? _availableCommands.Max(c => c.Name.Length) : 0;
-
-            Out.WordWrap("The available commands are:\n");
-
-            foreach (var command in _availableCommands)
-            {
-                Out.WordWrap(String.Format("{0," + -maximumCommandNameLength + "}{1}", command.Name, spacer), spacer.Length);
-                Out.WordWrap(String.Format("{0}\n", command.Description), Console.CursorLeft);
-            }
-        }
 
         public CommandRunner WithDependencyResolver(IDependencyResolver resolver)
         {
             _resolver = resolver;
             return this;
-        }
-
-        public string GetUsagePattern(OptionSet optionSet)
-        {
-            var output = new StringBuilder();
-
-            // Generate usage.
-            if (optionSet != null)
-            {
-                foreach (Option o in _optionSet)
-                {
-                    if (o.OptionValueType == OptionValueType.None)
-                    {
-                        output.AppendFormat("[--{0}]", o.Prototype);
-                    }
-                    if (o.OptionValueType == OptionValueType.Required)
-                    {
-                        output.AppendFormat("[--{0}VALUE]", o.Prototype);
-                    }
-
-                    output.Append(" ");
-                }
-            }
-
-            return output.Append("COMMAND ACTION [ARGS]").ToString();
         }
 
         public CommandRunner WithCommandsFromType<T>()
--- src/Synoptic/HelpUtilities/CommandLineHelpGenerator.cs
+++ src/Synoptic/HelpUtilities/CommandLineHelpGenerator.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Mono.Options;
+using Synoptic.ConsoleUtilities;
 
 namespace Synoptic.HelpUtilities
 {
@@ -7,6 +13,55 @@ namespace Synoptic.HelpUtilities
         public static CommandLineHelp Generate(IEnumerable<CommandAction> commandActions)
         {
             return new CommandLineHelp(commandActions);
+        }
+
+        private string GetUsagePattern(IEnumerable<Option> optionSet)
+        {
+            var output = new StringBuilder();
+
+            // Generate usage.
+            if (optionSet != null)
+            {
+                foreach (Option o in optionSet)
+                {
+                    if (o.OptionValueType == OptionValueType.None)
+                    {
+                        output.AppendFormat("[--{0}]", o.Prototype);
+                    }
+                    if (o.OptionValueType == OptionValueType.Required)
+                    {
+                        output.AppendFormat("[--{0}VALUE]", o.Prototype);
+                    }
+
+                    output.Append(" ");
+                }
+            }
+
+            return output.Append("COMMAND ACTION [ARGS]").ToString();
+        }
+
+        public void ShowCommandHelp(IEnumerable<Command> availableCommands, OptionSet optionSet)
+        {
+            var processName = Process.GetCurrentProcess().ProcessName;
+            var usagePreamble = String.Format("Usage: {0} ", processName);
+            Out.WordWrap(usagePreamble);
+
+            var usagePattern = GetUsagePattern(optionSet);
+            Out.WordWrap(usagePattern, usagePreamble.Length);
+            Console.WriteLine("\n");
+
+            const int spacingWidth = 3;
+
+            var spacer = new string(' ', spacingWidth);
+            var maximumCommandNameLength = availableCommands.Count() > 0 ? availableCommands.Max(c => c.Name.Length) : 0;
+
+            Out.WordWrap("The available commands are:\n");
+
+            foreach (var command in availableCommands)
+            {
+                Out.WordWrap(String.Format("{0," + -maximumCommandNameLength + "}{1}", command.Name, spacer), spacer.Length);
+                Out.WordWrap(String.Format("{0}\n", command.Description), Console.CursorLeft);
+            }
         }
     }
 }
