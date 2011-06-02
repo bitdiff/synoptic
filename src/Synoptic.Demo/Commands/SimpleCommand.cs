89e26724ff4a6566a43f11472f9a957aa1f45f06
Ziya Suzen <z@bitdiff.com>
2011-06-02 14:06:05 +0100

    Simple command test

---
+++ src/Synoptic.Demo/Commands/SimpleCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Synoptic.Demo.Commands
+{
+    [Command]
+    public class SimpleCommand
+    {
+        [CommandAction]
+        public void Go([CommandParameter("ss|s")]string someString, int someNumber)
+        {
+            Console.Out.WriteLine("someString: {0}", someString);
+            Console.Out.WriteLine("someNumber: {0}", someNumber);
+        }
+    }
+}
