85894443dded3b298008744b4c2f33334816d9cd
Ziya Suzen <z@bitdiff.com>
2011-08-09 22:09:24 +0100

    Use release mode

--- build/build.bat
+++ build/build.bat
@@ -7,7 +7,7 @@ set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR%
 cd ..\src
 
-msbuild /v:m /nologo /p:Configuration=Debug /t:clean /t:build
+msbuild /v:m /nologo /p:Configuration=Release /t:clean /t:build
 
 cd %CURR_DIR%
 
